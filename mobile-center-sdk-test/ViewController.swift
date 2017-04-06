114acc5c9bb112e766aa4212e8a7915636dabbf6
denis kudelin <denis.kudelin@akvelon.com>
2017-04-06 05:55:22 -0700

    Added test controls

--- mobile-center-sdk-test/ViewController.swift
+++ mobile-center-sdk-test/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testButton: UIButton!
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func testButtonTouchDown(_ sender: UIButton) {
+        
+    }
 
 }
 
